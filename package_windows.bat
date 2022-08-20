3d326fc1d3c9a008d94c3ed7622d821ef6467d95
Aaron Christiansen <aaronc20000@gmail.com>
2022-08-20 23:12:43 +0100

    Don't package Sketch file on Windows

--- package_windows.bat
+++ package_windows.bat
@@ -4,4 +4,4 @@ md "%0\..\build" 2> nul
 if exist "%0\..\build\windows" del "%0\..\build\windows"
 md "%0\..\build\windows"
 
-ocra ./src/main.rb res/** --windows --output "%0\..\build\windows\MiniMallMaker.exe"
+ocra ./src/main.rb res/audio res/customers res/units res/logo.png --windows --output "%0\..\build\windows\MiniMallMaker.exe"
