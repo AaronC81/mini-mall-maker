0ab192cb286dc2e71723b5eda799914b5a5cb91d
Aaron Christiansen <aaronc20000@gmail.com>
2022-08-20 23:03:16 +0100

    Update packaging scripts

--- package_windows.bat
+++ package_windows.bat
@@ -4,4 +4,4 @@ md "%0\..\build" 2> nul
 if exist "%0\..\build\windows" del "%0\..\build\windows"
 md "%0\..\build\windows"
 
-ocra ./src/main.rb res/** --windows --output "%0\..\build\windows\GosuGameJam3.exe"
+ocra ./src/main.rb res/** --windows --output "%0\..\build\windows\MiniMallMaker.exe"
