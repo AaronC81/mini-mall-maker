7a0cddc717101c8bef6c458075b4aa0c00772407
Aaron Christiansen <aaronc20000@gmail.com>
2022-08-20 21:39:45 +0100

    Add unit names on hover

--- src/mall/unit.rb
+++ src/mall/unit.rb
@@ -1,4 +1,5 @@
 require_relative '../engine/entity'
+require_relative '../ui/tooltip'
 
 module GosuGameJam3
   # A store, facility, etc in the mall. Must be used through a derived class.
@@ -29,6 +30,7 @@ module GosuGameJam3
     def budget; self.class.budget; end
     def purchase_chance; self.class.purchase_chance; end
     def purchase_range; self.class.purchase_range; end
+    def unit_name; self.class.unit_name; end
     
     # The images of this unit. This is derived from the class' name, by converting it into snake
     # case.
@@ -70,8 +72,12 @@ module GosuGameJam3
     # The range of prices which a customer may pay when buying something from this unit. 
     def self.purchase_range; raise 'abstract'; end
 
+    # The name of this unit.
+    def self.unit_name; raise 'abstract'; end
+
     # A convenience method to define properties of this unit.
-    def self.derive_unit(_departments, _budget, _build_cost, _purchase_chance, _purchase_range)
+    def self.derive_unit(_name, _departments, _budget, _build_cost, _purchase_chance, _purchase_range)
+      define_singleton_method(:unit_name) { _name }
       define_singleton_method(:departments) { _departments }
       define_singleton_method(:budget) { _budget }
       define_singleton_method(:build_cost) { _build_cost }
@@ -93,6 +99,7 @@ module GosuGameJam3
         position.x,
         position.y,
       )
+      draw_tooltip
     end
 
     def draw_bg
@@ -106,5 +113,11 @@ module GosuGameJam3
     def slots_occupied
       (offset...(offset + size)).to_a
     end
+
+    # Tooltip implementation
+    include Tooltip
+    def width; size * Mall::SLOT_WIDTH; end
+    def height; Mall::FLOOR_HEIGHT; end
+    def tooltip; unit_name; end
   end
 end
--- src/mall/units.rb
+++ src/mall/units.rb
@@ -7,57 +7,57 @@ module GosuGameJam3
     B = Customer::Preferences::Budget
 
     class DesignerClothes < Unit
-      derive_unit([D::Fashion], B::HighEnd, 1500, 0.3, 100..600)
+      derive_unit("Designer Clothes", [D::Fashion], B::HighEnd, 1500, 0.3, 100..600)
     end
     class DiscountClothes < Unit
-      derive_unit([D::Fashion], B::Discount, 800, 0.9, 10..30)
+      derive_unit("Discount Clothes", [D::Fashion], B::Discount, 800, 0.9, 10..30)
     end
     class Shoes < Unit
-      derive_unit([D::Fashion], B::Intermediate, 500, 0.4, 40..80)
+      derive_unit("Shoes", [D::Fashion], B::Intermediate, 500, 0.4, 40..80)
     end
 
     class HighEndTechnology < Unit
-      derive_unit([D::Technology], B::HighEnd, 1500, 0.1, 500..2000)
+      derive_unit("High-end Technology", [D::Technology], B::HighEnd, 1500, 0.1, 500..2000)
     end
     class UsedTechnology < Unit
-      derive_unit([D::Technology], B::Intermediate, 600, 0.4, 20..150)
+      derive_unit("Used Technology", [D::Technology], B::Intermediate, 600, 0.4, 20..150)
     end
     class PhoneCases < Unit
-      derive_unit([D::Technology], B::Discount, 300, 0.9, 5..10)
+      derive_unit("Phone Cases", [D::Technology], B::Discount, 300, 0.9, 5..10)
     end
 
     class GeneralToys < Unit
-      derive_unit([D::Toys], B::Discount, 800, 0.6, 10..50)
+      derive_unit("Toys", [D::Toys], B::Discount, 800, 0.6, 10..50)
     end
     class BuildingBlockToys < Unit
-      derive_unit([D::Toys], B::HighEnd, 1300, 0.5, 50..200)
+      derive_unit("Building Block Toys", [D::Toys], B::HighEnd, 1300, 0.5, 50..200)
     end
     class VideoGames < Unit
-      derive_unit([D::Toys], B::Intermediate, 600, 0.7, 10..50)
+      derive_unit("Video Games", [D::Toys], B::Intermediate, 600, 0.7, 10..50)
     end
 
     class Pharmacy < Unit
-      derive_unit([D::Health], B::Discount, 500, 0.9, 1..5)
+      derive_unit("Pharmacy", [D::Health], B::Discount, 500, 0.9, 1..5)
     end
     class Cosmetics < Unit
-      derive_unit([D::Health], B::Intermediate, 800, 0.5, 10..50)
+      derive_unit("Cosmetics", [D::Health], B::Intermediate, 800, 0.5, 10..50)
     end
     class LuxurySoap < Unit
-      derive_unit([D::Health], B::HighEnd, 1000, 0.6, 20..60)
+      derive_unit("Luxury Soap", [D::Health], B::HighEnd, 1000, 0.6, 20..60)
     end
 
     class Bakery < Unit
-      derive_unit([D::Food], B::Discount, 400, 0.7, 1..4)
+      derive_unit("Bakery", [D::Food], B::Discount, 400, 0.7, 1..4)
     end
     class Donuts < Unit
-      derive_unit([D::Food], B::Intermediate, 400, 0.9, 1..2)
+      derive_unit("Donuts", [D::Food], B::Intermediate, 400, 0.9, 1..2)
     end
     class FineDining < Unit
-      derive_unit([D::Food], B::HighEnd, 1600, 0.4, 50..100)
+      derive_unit("Fine Dining", [D::Food], B::HighEnd, 1600, 0.4, 50..100)
     end
 
     class Elevator < Unit
-      derive_unit([D::Special], B::Any, 200, 0, 0..0)
+      derive_unit("Elevator", [D::Special], B::Any, 200, 0, 0..0)
     end
   end
 end
